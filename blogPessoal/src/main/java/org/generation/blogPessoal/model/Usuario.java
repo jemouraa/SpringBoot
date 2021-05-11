08f1c5860962b114cfacf4d79a42b2185d263836
jemouraa <jecruz12@gmail.com>
2021-05-11 11:50:30 -0300

    Exercicios 07-05

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -0,0 +1,42 @@
+package org.generation.blogPessoal.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String usuario;
+	private String senha;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public Long getId() {
+		return id;
+	}
+	
+
+	
+}
